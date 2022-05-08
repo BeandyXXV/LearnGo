91ce9aad7a7bedc5cbb2e3e73104be7da0fc1773
Beandyxxv <andrew25wang@163.com>
2022-05-08 16:00:57 +0800

    data type done

--- pkg/basicSyntax/varThings.go
+++ pkg/basicSyntax/varThings.go
@@ -66,11 +66,11 @@ func Var_learn() {
 	*/
 
 	// 打印上述变量的值
-	println(firstVar, secondVar, thirdVar, fourthVar, fifthVar, sixthVar,
+	fmt.Println(firstVar, secondVar, thirdVar, fourthVar, fifthVar, sixthVar,
 		seventhVar.name, seventhVar.age, eighthVar, ninthVar, tenthVar)
-	println(damage)
-	println(conn, err, conn1)
-	println(num_1, num_2)
+	fmt.Println(damage)
+	fmt.Println(conn, err, conn1)
+	fmt.Println(num_1, num_2)
 }
 
 func Data_type() {
@@ -105,10 +105,10 @@ func Data_type() {
 	// 使用 printf 函数对浮点数进行打印的时候，可以使用 %f 或 %g 格式，%f 格式会输出最精确的结果，%g 格式会输出最简短的结果
 	fmt.Printf("%f, %.2f, %g\n", math.Pi, math.Pi, math.Pi)
 
-	println("浮点数部分：")
-	println(a, b)
-	println(Avogadro, Planck)
-	println(math.Pi)
+	fmt.Println("浮点数部分：")
+	fmt.Println(a, b)
+	fmt.Println(Avogadro, Planck)
+	fmt.Println(math.Pi)
 
 	/*
 		// 复数变量
@@ -125,11 +125,11 @@ func Data_type() {
 	comp_easy := complex(x, y)
 	// 复数之间可以使用 == 判断相等，也可以使用 != 判断不相等，当且仅当实部和虚部都相等时，两个复数才相等。
 
-	println("复数部分：")
-	println(comp, comp_easy)
+	fmt.Println("复数部分：")
+	fmt.Println(comp, comp_easy)
 
 	// 对于一个复数，可以使用 real 和 imag 方法来获取它的实部和虚部
-	println("实部：", real(comp), "\t", "虚部：", imag(comp))
+	fmt.Println("实部：", real(comp), "\t", "虚部：", imag(comp))
 
 	/*
 		// 布尔型变量
@@ -137,21 +137,21 @@ func Data_type() {
 		// if 和 for 循环中的条件语句，只能使用布尔型变量。同时 == 和 != 等比较操作符也会返回布尔型变量。
 		// 一元操作符 ! 可以将布尔型变量转换为相反的值。
 	*/
-	println("布尔型变量：")
-	println((true == false) == false)
+	fmt.Println("布尔型变量：")
+	fmt.Println((true == false) == false)
 
 	// 布尔值可以用 &&（AND） 和 ||（OR） 操作符来组合，如果运算符左侧的值已经可以确定整个布尔表达式的值，那么右侧的运算符就不会被执行。
 	// 下例中，s[0] 操作如果用于空字符串，会导致 panic 异常。但运算符左侧已经确定了整个表达式的值，右侧运算符不执行，所以不会报错。
 	s := ""
-	println(s != "" && s[0] == 'x')
+	fmt.Println(s != "" && s[0] == 'x')
 
 	// 因为 && 的优先级高于 ||（&& 对应逻辑乘法，|| 对应逻辑加法），可以视优先级来决定是否加上括号进行比较。
 	c := "b"
-	println("a" <= c && c <= "z" || "A" <= c && c <= "Z" || "0" <= c && c <= "9")
+	fmt.Println("a" <= c && c <= "z" || "A" <= c && c <= "Z" || "0" <= c && c <= "9")
 
 	// Go 语言中不允许将整形变量强制转换为布尔形变量。
 	// var n bool
-	// println(int(n) * 2)
+	// fmt.Println(int(n) * 2)
 
 	/*
 		// 字符串变量
@@ -161,23 +161,24 @@ func Data_type() {
 
 	// 可以使用双引号""来定义字符串，字符串中可以使用转义字符来实现换行、缩进等效果，常用的转义字符包括：
 	// \n 换行 \r 回车 \t 水平制表符 \v 垂直制表符 \\ 反斜杠 \' 单引号 \" 双引号 \u 字符的 Unicode 编码
+	fmt.Println("字符串变量：")
 	var str = "C语言中文网\nGo语言教程"
-	println(str)
+	fmt.Println(str)
 
 	// 比较运算符是通过在内存中按字节比较来实现字符串的比较，所以比较的结果是字符串自然编码的顺序。
 	var str_01 string = "str_test_01"
 	var str_02 string = "str_test_02"
-	println(str_01 == str_02, len(str_01), len(str_02), len(str))
+	fmt.Println(str_01 == str_02, len(str_01), len(str_02), len(str))
 
 	// 字符串的内容（纯字节）可以通过标准索引法来获取，在方括号 [] 中写入索引，从 0 开始计数。此方法只适用于纯ASCII字符串。
-	println(str_01[0])
+	fmt.Println(str_01[0])
 	// 获取字符串中某个字节的地址属于非法行为，例如 &srt[0]，因为字符串是一个纯字节数组，不能获取字符串中的字节地址。
 
 	// 两个字符串可以通过 + 操作符进行拼接，拼接的结果是一个新的字符串，而不会改变原来的字符串。
 	str_03 := "Beginning of the string " + "second part of the string"
 	str_04 := "hel" + "lo,"
 	str_04 += "world!"
-	println(str_03, str_04)
+	fmt.Println(str_03, str_04)
 
 	// 在Go语言中，使用双引号书写字符串的方式是字符串常见表达方式之一，被称为字符串字面量（string literal），这种双引号字面量不能跨行.
 	// 如果想要在源码中嵌入一个多行字符串时，就必须使用 ` 反引号
@@ -188,6 +189,45 @@ func Data_type() {
 第三行
 \r\n
 `
-	println(str_05)
+	fmt.Print(str_05)
 
+	/*
+		// Go 语言字符类型
+		// Go 语言的字符有以下两种：uint8 和 rune。
+		// uint8 也可以称作 byte 型，可以表示 ASCII 码的一个字符
+		// rune 代表一个 UTF-8 字符，当需要处理中文、日文等复合字符时，就需要用到 rune 类型，等价于 int32。
+	*/
+	fmt.Println("字符类型变量")
+	var ch_byte byte = 65
+	var ch_byte_01 int8 = 'a'
+	fmt.Print(ch_byte, ch_byte_01)
+
+	// Go 语言同样支持 Unicode 编码，因此字符同样称为 Unicode 代码点或者 runes，并在内存中用 int 表示。
+	// 在文档中，一般使用格式 U+hhhh 表示，其中 h 表示一个 16 进制数。
+	// 在书写 Unicode 字符时，需要在 16 进制前面加上前缀 \u 或者 \U。
+	// 因为 Unicode 至少占用 2 个字节，所以我们使用 int16 或者 int 类型来表示。
+	// 如果需要使用到 4 字节，则使用 \u 前缀，如果需要使用到 8 个字节，则使用 \U 前缀。
+	var ch int = '\u0041'
+	var ch2 int = '\u03B2'
+	var ch3 int = '\U00101234'
+	fmt.Printf("%d - %d - %d\n", ch, ch2, ch3) // integer
+	fmt.Printf("%c - %c - %c\n", ch, ch2, ch3) // character
+	fmt.Printf("%X - %X - %X\n", ch, ch2, ch3) // UTF-8 bytes
+	fmt.Printf("%U - %U - %U\n", ch, ch2, ch3) // UTF-8 code point
+
+	// Unicode 包中内置了一些用于测试字符的函数
+	// 判断是否为字母：unicode.IsLetter(ch)
+	// 判断是否为数字：unicode.IsDigit(ch)
+	// 判断是否为空白符号：unicode.IsSpace(ch)
+
+	/*
+		// 数据类型的转换
+	*/
+	// 在必要及可行的情况下，一个类型的值可以被转换成另一个类型。Go 语言不存在隐式类型转换，所有类型转换必须显式声明
+	a_float := 10.0
+	a_int := int(a_float)
+	fmt.Println(a_int)
+	// 类型转换只能在定义正确的情况下转换成功，当从一个取值范围较大的类型转换到一个取值范围较小的类型的时候，会出现精度丢失的问题。
+	// 只有相同底层类型的变量之间可以进行相互转换，如 int16 -> int32。不同底层类型的变量相互转换时会发生编译错误。如 bool -> int
+	// 浮点数在转换为整数时，会去掉所有小数部分只保留整数部分。
 }
